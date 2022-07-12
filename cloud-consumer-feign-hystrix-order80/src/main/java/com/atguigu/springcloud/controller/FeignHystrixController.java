ae1c8be97d2b21a077a67a217a349a62534751cd
huangwei <1142488172@qq.com>
2022-07-12 23:12:05 +0800

    服务端和客户端服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/FeignHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/FeignHystrixController.java
@@ -1,8 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentFeignHysitrxService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +12,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "paymentTimeOutDefaultProperties")
 public class FeignHystrixController {
 
     @Resource
@@ -19,15 +20,18 @@ public class FeignHystrixController {
 
 
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    @HystrixCommand
     public String paymentInfo_OK(@PathVariable("id") Integer id) {
+//        int i  = 10/0;
         String result = paymentHystrixService.paymentInfoHysitrx(id);
         return result;
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo(id);
         return result;
@@ -35,6 +39,10 @@ public class FeignHystrixController {
 
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
         return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+    public String paymentTimeOutDefaultProperties() {
+        return "我是消费者80,paymentTimeOutDefaultProperties提示：对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
 
 }
