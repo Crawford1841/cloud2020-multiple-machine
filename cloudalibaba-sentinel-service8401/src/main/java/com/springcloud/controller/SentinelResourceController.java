f06ceca99d05edf57ccfc48a7432b32d4c007bcf
huangwei <1142488172@qq.com>
2022-07-22 11:11:51 +0800

    Sentinel自定义异常

--- cloudalibaba-sentinel-service8401/src/main/java/com/springcloud/controller/SentinelResourceController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/springcloud/controller/SentinelResourceController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.springcloud.handler.CustomerBlockHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,30 @@ public class SentinelResourceController {
     public CommonResult handleException(BlockException exception)
     {
         return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl()
+    {
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+    }
+
+    /**
+     * 自定义通用的限流处理逻辑，
+     blockHandlerClass = CustomerBlockHandler.class
+     blockHandler = handleException2
+     上述配置：找CustomerBlockHandler类里的handleException2方法进行兜底处理
+     */
+    /**
+     * 自定义通用的限流处理逻辑
+     */
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class, blockHandler = "handleException2")
+    public CommonResult customerBlockHandler()
+    {
+        return new CommonResult(200,"按客户自定义限流处理逻辑");
     }
 
 }
